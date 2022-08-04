120851ee66496080fdd0ad7df9ccf2d9f1a175f8
Tolegen Timur <demigodkz@gmail.com>
2022-08-04 21:23:34 +0600

    feat: add carStart, carStop functions

--- src/util/helperFunctions.ts
+++ src/util/helperFunctions.ts
@@ -1,10 +1,12 @@
+import { AnimationControls } from 'framer-motion';
 import { carBrand, carModal } from '../data';
-import { ICarSpeed, ICarResult } from '../types';
+import { ICarSpeed, ICarResult, ICarWinner } from '../types';
 
 const CONTAINER_WIDTH = 1536;
 const CAR_WIDTH = 210;
 const CONVERT_M_TO_KM = 1000;
 const CARS_PER_PAGE = 7;
+
 const getRandomNumber = (length: number) => {
   const randomNumber = Math.round(Math.random() * 100);
   return randomNumber > length ? length : randomNumber;
@@ -13,6 +15,10 @@ const getRandomName = (lengthBrand: number, lengthModal: number) => {
   return `${carBrand[getRandomNumber(lengthBrand)]} ${carModal[getRandomNumber(lengthModal)]}`;
 };
 const getRandomColor = () => `#${String(Math.random().toString(16)).slice(2, 8)}`;
+const getWidth = (width: number) => (width > CONTAINER_WIDTH ? CONTAINER_WIDTH : width) - CAR_WIDTH;
+const getTime = ({ distance, velocity }: ICarSpeed) => ({
+  duration: distance / (velocity * CONVERT_M_TO_KM)
+});
 const getRaceData = async (id: string, status: string) => {
   const response = await fetch(`http://localhost:3001/engine?id=${id}&status=${status}`, {
     method: 'PATCH'
@@ -29,9 +35,35 @@ const getResult = async (id: string) => {
   const data: ICarResult = await response.json();
   return data.success;
 };
-const getWidth = (width: number) => (width > CONTAINER_WIDTH ? CONTAINER_WIDTH : width) - CAR_WIDTH;
-const getTime = ({ distance, velocity }: ICarSpeed) => ({
-  duration: distance / (velocity * CONVERT_M_TO_KM)
-});
+const carStart = async (
+  controls: AnimationControls,
+  id: string,
+  width: number,
+  setWinner: React.Dispatch<React.SetStateAction<ICarWinner[]>>
+) => {
+  const carRaceData = await getRaceData(id, 'started');
+  controls.start({ x: getWidth(width), transition: getTime(carRaceData) });
+  const result = await getResult(id);
+  if (result) setWinner((state) => [...state, { id, time: carRaceData.velocity }]);
+  if (!result) {
+    setWinner((state) => [...state, { id, time: 0 }]);
+    controls.stop();
+  }
+};
+const carStop = async (controls: AnimationControls, id: string) => {
+  await getRaceData(id, 'stopped');
+  controls.stop();
+  controls.start({ x: 0, transition: { duration: 2 } });
+};
 const getCount = (num = 1) => Math.ceil(num / CARS_PER_PAGE);
-export { getRandomName, getRandomColor, getRaceData, getResult, getWidth, getTime, getCount };
+export {
+  getRandomName,
+  getRandomColor,
+  getRaceData,
+  getResult,
+  getWidth,
+  getTime,
+  getCount,
+  carStart,
+  carStop
+};
