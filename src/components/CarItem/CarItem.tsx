f4847c9f317e972bb4f06f573411ac11e0166f08
Tolegen Timur <demigodkz@gmail.com>
2022-08-06 02:07:22 +0600

    feat: add hook useRaceAnimation

--- src/components/CarItem/CarItem.tsx
+++ src/components/CarItem/CarItem.tsx
@@ -1,28 +1,17 @@
-import { FC, memo, useCallback, useLayoutEffect } from 'react';
+import { FC, memo } from 'react';
 import { Stack, Box, SvgIcon } from '@mui/material';
-import { motion, useAnimationControls } from 'framer-motion';
+import { motion } from 'framer-motion';
 
 import CarItemControls from './CarItemControls';
 
-import { carStart, carStop } from '../../util/helperFunctions';
 import { ReactComponent as finish } from '../../assets/finish.svg';
 import { ReactComponent as f1 } from '../../assets/f1.svg';
 import { finishStyle } from '../../util/styles';
 import { ICarItem } from '../../types';
+import useRaceAnimation from '../hooks/useRaceAnimation';
 
 const CarItem: FC<ICarItem> = ({ id = '', name, color, race, setWinner }) => {
-  const { innerWidth: width } = window;
-  const controls = useAnimationControls();
-  const start = useCallback(
-    () => carStart(controls, id, name, width, setWinner, race),
-    [controls, setWinner, id, name, width, race]
-  );
-  const stop = useCallback(() => carStop(controls, id), [controls, id]);
-  useLayoutEffect(() => {
-    if (race === 'started') start();
-    if (race === 'stopped') stop();
-  }, [start, stop, race]);
-
+  const { controls, start, stop } = useRaceAnimation(id, name, race, setWinner);
   return (
     <Stack>
       <CarItemControls id={id} race={race} name={name} carStart={start} carStop={stop} />
---
+++ src/components/hooks/useRaceAnimation.ts
@@ -0,0 +1,26 @@
+import { useCallback, useLayoutEffect } from 'react';
+import { useAnimationControls } from 'framer-motion';
+
+import { carStart, carStop } from '../../util/helperFunctions';
+import { ICarWinner } from '../../types';
+
+const useRaceAnimation = (
+  id: string,
+  name: string,
+  race: string,
+  setWinner: React.Dispatch<React.SetStateAction<ICarWinner[]>>
+) => {
+  const { innerWidth: width } = window;
+  const controls = useAnimationControls();
+  const start = useCallback(
+    () => carStart(controls, id, name, width, setWinner, race),
+    [controls, setWinner, id, name, width, race]
+  );
+  const stop = useCallback(() => carStop(controls, id), [controls, id]);
+  useLayoutEffect(() => {
+    if (race === 'started') start();
+    if (race === 'stopped') stop();
+  }, [start, stop, race]);
+  return { controls, start, stop };
+};
+export default useRaceAnimation;
