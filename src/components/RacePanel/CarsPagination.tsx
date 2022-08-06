50fa92fd4bab3dd93d5e0183a886f10bc155b0fc
Tolegen Timur <demigodkz@gmail.com>
2022-08-06 22:20:09 +0600

    feat: add correct behavior when deleting all elements

--- src/components/RacePanel/CarsPagination.tsx
+++ src/components/RacePanel/CarsPagination.tsx
@@ -1,13 +1,18 @@
-import { FC, Fragment, memo } from 'react';
+import { FC, Fragment, memo, useEffect } from 'react';
 import { Stack, Typography, Pagination, Divider } from '@mui/material';
 
 import { ICarsPagination } from '../../types';
 import { getCount } from '../../util/helperFunctions';
 
-const CarsPagination: FC<ICarsPagination> = ({ data, page, setPage }) => {
-  const handleChange = (e: React.ChangeEvent<unknown>, value: number) => {
-    setPage(value);
-  };
+const CarsPagination: FC<ICarsPagination> = ({ data, race, setRace, page, setPage }) => {
+  useEffect(() => {
+    if (!data?.apiResponse.length && page > 1) {
+      const currentPage = Number(localStorage.getItem('_garagePage'));
+      setPage(data?.totalCount ? getCount(data?.totalCount) : currentPage);
+    }
+  }, [data?.apiResponse.length, data?.totalCount, page, setPage]);
+
+  if (!data) return null;
   return (
     <Fragment>
       <Divider sx={{ mt: 2 }}>
@@ -22,10 +27,14 @@ const CarsPagination: FC<ICarsPagination> = ({ data, page, setPage }) => {
         <Pagination
           count={getCount(data?.totalCount)}
           page={page}
-          onChange={handleChange}
+          onChange={(_, value) => {
+            setPage(value);
+            setRace('');
+          }}
           shape="rounded"
           showFirstButton
           showLastButton
+          disabled={race === 'started'}
         />
         <Typography variant="h6" component="div">
           Garage: ({data?.totalCount})
