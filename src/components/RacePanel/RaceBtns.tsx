2bc4549cd7417703d84bb5c8724e620765cb1cdd
Tolegen Timur <demigodkz@gmail.com>
2022-08-06 22:16:54 +0600

    fix: remove unused state

--- src/components/RacePanel/RaceBtns.tsx
+++ src/components/RacePanel/RaceBtns.tsx
@@ -1,10 +1,9 @@
-import { FC, Fragment, useState, memo } from 'react';
+import { FC, Fragment, memo } from 'react';
 import { Divider, Typography, Stack, Button } from '@mui/material';
 
 import { IRaceBtns } from '../../types';
 
 const RaceBtns: FC<IRaceBtns> = ({ setRace, race }) => {
-  const [disabled, setDisabled] = useState(false);
   return (
     <Fragment>
       <Divider sx={{ mt: 2 }}>
@@ -14,26 +13,20 @@ const RaceBtns: FC<IRaceBtns> = ({ setRace, race }) => {
       </Divider>
       <Stack spacing={2} direction="row" sx={{ alignItems: 'center', justifyContent: 'center' }}>
         <Button
-          onClick={() => {
-            setRace('started');
-            setDisabled(true);
-          }}
+          onClick={() => setRace('started')}
           size="small"
           variant="contained"
           sx={{ color: '#F3E600' }}
-          disabled={disabled}
+          disabled={race === 'started' || race === 'pending'}
         >
           Race
         </Button>
         <Button
-          onClick={() => {
-            setRace('stopped');
-            setDisabled(false);
-          }}
+          onClick={() => setRace('stopped')}
           size="small"
           variant="contained"
           sx={{ color: '#F3E600' }}
-          disabled={!!race || !disabled}
+          disabled={race === 'started'}
         >
           Recet
         </Button>
